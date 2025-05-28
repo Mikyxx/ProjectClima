24932e3272b1bd8538a47e22fa8a3844a396f5be
Miqueias Barros <103081596+Mikyxx@users.noreply.github.com>
2025-05-28 12:26:27 -0300

    Second Commit

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 document.querySelector('.busca').addEventListener('submit', async (event)=>{
     event.preventDefault();
 
-    let input = document.querySelector('#searchInput').ariaValueMax;
+    let input = document.querySelector('#searchInput').value;
     
-    if(input != '') {
+    if(input !== '') {
+        clearinfo();
         showWarning('Carregando...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=5ce4eb3da98cb28ffc51b0e9252ae057&units=metric&lang=pt_br`;
 
         let results = await fetch(url);
         let json = await results.json();
-    }
 
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+        } else {
+            clearinfo();
+
+            showWarning('Não encontramos esta localização.');
+        } 
+    } else {
+        clearinfo();
+    }
 });
 
+function showInfo(json) {
+    showWarning('');
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC<sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearinfo(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
 function showWarning(msg) {
-    document.querySelector('.aviso').innetHTML = msg;
+    document.querySelector('.aviso').innerHTML = msg;
 }
